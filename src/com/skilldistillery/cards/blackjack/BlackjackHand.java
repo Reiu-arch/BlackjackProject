e4ad510bd220e674c66df2ffb4f3502a597bb0b7
Reiu-arch <vidaldominguez60@gmail.com>
2024-11-17 18:39:25 -0700

    added three new methods to blackjackhand plus updated comments

--- src/com/skilldistillery/cards/blackjack/BlackjackHand.java
+++ src/com/skilldistillery/cards/blackjack/BlackjackHand.java
@@ -6,18 +6,35 @@ import com.skilldistillery.cards.common.Card;
 public class BlackjackHand extends AbstractHand {
 
 	public int getHandValue;
-
-	public void addCard(Card card) {
-		
-	}
-
+	
+	
+	//for each card in hand the value is added back into the sum of said hand
 	@Override
 	public int getHandValue() {
-		return 0;
+		int handValue = 0;
+		for (Card card : cardsInHand) {
+			handValue += card.getValue(handValue);
+		}
+		return handValue;
 	}
 	
-	//need an isBust method gethandval => 21
-	//need an isBlackjack gethandval == 21
-	//need to flesh out gethandvalue.....maybe a for each card add the card value and return it to this.public int
+	//if number in hand is equal to 21 then it is blackjack, if not then not blackjack
+	public boolean isBust() {
+		if (getHandValue == 21) {
+			return true;
+		}
+		return false;
+	}
+	
+	//if number in hand is over 21 then handValue is bust, if under or equal to 21 then not bust.
+	public boolean Blackjack() {
+		if(getHandValue > 21) {
+			return true;
+		}
+		return false;
+	}
+	//need an isBust method gethandval => 21 either is or is not (boolean)
+	//need an isBlackjack gethandval == 21  "                            "
+	//need to flesh out gethandvalue.....maybe a for each card add the card value and return handvalue to gethandvalue
 
 }
